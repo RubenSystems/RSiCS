5026052e7145953f9c166302ee8388eff22e6e21
RubenSystems <ruben.ticehurst-james@kcl.ac.uk>
2022-11-11 22:28:03 +0000

    const void * for context to make sure rsics won't edit it

--- definition/observe.h
+++ definition/observe.h
@@ -20,9 +20,9 @@ enum ObserveResponse {
 
 // Attach event listener
 
-void observe_with_context(struct Computer *, char *, void *, void (*recieved_message)(void *, struct Computer, const char *, int));
+void observe_with_context(struct Computer *, char *, const void *, void (*recieved_message)(const void *, struct Computer, const char *, int));
 
-void observe(struct Computer *, char *, void (*recieved_message)(void *, struct Computer, const char *, int));
+void observe(struct Computer *, char *, void (*recieved_message)(const void *, struct Computer, const char *, int));
 
 enum ObserveResponse _recieve_packet(struct Packet *, struct Computer *, struct Computer *);
 
